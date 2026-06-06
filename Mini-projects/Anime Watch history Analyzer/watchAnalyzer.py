c881d04d98de74a820c5f8678fe337daf7cea855
Jamima Afroz Usha <jamimaafroz123@gmail.com>
2026-06-06 15:42:38 +0600

    Feat: Implemented basic functions and graph

--- Mini-projects/Anime Watch history Analyzer/watchAnalyzer.py
+++ Mini-projects/Anime Watch history Analyzer/watchAnalyzer.py
@@ -4,3 +4,47 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('anime_list.csv')
 df = pd.DataFrame(data)
 print(df)
+
+print(df["Rating"].mean())
+print(df["Episodes"].sum())
+
+top_anime = df.sort_values(
+    by="Rating",
+    ascending=False
+)
+
+print(top_anime)
+
+genre_count = df["Genre"].value_counts()
+
+print(genre_count)
+
+
+genre_rating = df.groupby("Genre")["Rating"].mean()
+
+print(genre_rating)
+
+plt.pie(
+    genre_count,
+    labels=genre_count.index,
+    autopct="%1.1f%%"
+)
+
+plt.title("Anime Genre Distribution")
+plt.savefig('Anime Genre Distribution')
+plt.show()
+
+
+top5 = df.sort_values(
+    by="Rating",
+    ascending=False
+).head(5)
+
+plt.bar(
+    top5["Anime"],
+    top5["Rating"]
+)
+
+plt.title("Top Rated Anime")
+plt.show()
+
